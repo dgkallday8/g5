e8259feee5b99d598f9f1250ccd9e79cbe8730cc
enotdgk <enotdgk@mail.com>
2024-06-25 11:26:49 +0500

    basic 2 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlocksComponent } from './components/blocks/blocks.component'
+import { TableComponent } from './components/table/table.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'blocks',
+    component: BlocksComponent,
+  },
+  {
+    path: 'table',
+    component: TableComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'blocks',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { BlocksComponent } from './components/blocks/blocks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    BlocksComponent
   ],
   imports: [
     BrowserModule,
