420a07c7465d7d3af5d88240b766b4f62b08a94f
enotdgk <enotdgk@mail.com>
2024-06-25 14:59:12 +0500

    base layout for table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SearchComponent } from '../search/search.component'
+import { IUserData } from '../../common/interfaces'
+import { HttpService } from '../../services/http.service'
 
 @Component({
   selector: 'app-table',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  @ViewChild(SearchComponent)
+  public searchComponent!: SearchComponent;
 
+  public users: IUserData[] = []
+
+  public showToast = false;
+
+  constructor(private _httpService: HttpService) {}
+
+  public onUsersEmit(gotUsers: IUserData[] | null) {
+    if (gotUsers) {
+      this.users = this.users.concat(gotUsers)
+
+      if (!gotUsers.length && !this.users.length) {
+        this.showToast = true
+      }
+    } else {
+      this.users = []
+    }
+  }
+
+  public toUser(user: IUserData) {
+    console.log('toUser', user);
+  }
+
+  public onScroll() {
+    this._httpService.searchPage.next(this._httpService.searchPage.value + 1)
+    this.searchComponent.onSubmit()
+  }
 }
