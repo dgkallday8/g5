97606b50b178a404a7291401e323f12e7fa7a36b
enotdgk <enotdgk@mail.com>
2024-06-29 00:19:58 +0500

    guards added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard, publicGuard } from './core/guards/auth.guard'
 
 const routes: Routes = [
   {
     path: 'home',
+    canActivate: [authGuard],
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'auth',
+    canActivate: [publicGuard],
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
