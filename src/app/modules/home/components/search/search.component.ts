0f64af78c40d049b7166e577c8a366a4cdae7c26
enotdgk <enotdgk@mail.com>
2024-07-01 10:54:58 +0500

    unsub and toast position fixed

--- src/app/modules/home/components/search/search.component.ts
+++ src/app/modules/home/components/search/search.component.ts
@@ -1,18 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms'
 import { HttpService } from '../../../../core/services/http.service'
+import { ToastService } from '../../../../core/services/toast.service'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnDestroy {
   @Output() public usersEmitter = new EventEmitter();
+
+  private _sub$!: Subscription
 
   public valueControl = this._fb.control('');
 
-  constructor(private _httpService: HttpService, private _fb: FormBuilder) {}
+  constructor(private _httpService: HttpService, private _fb: FormBuilder, private _toastService: ToastService) {}
 
   public onSubmit(resetPage = false) {
     if (!this.valueControl.value) return;
@@ -28,10 +32,21 @@ export class SearchComponent {
       per_page: this._httpService.perPageValue,
     }
 
-    this._httpService.getUsers(filters).subscribe(res => {
-      if (res) {
-        this.usersEmitter.emit(res.items)
-      }
+    this._sub$ = this._httpService.getUsers(filters).subscribe({
+      next: (res) => {
+        if (res) {
+          this.usersEmitter.emit(res.items)
+        }
+      },
+      error: () => {
+        this._toastService.error('API rate limit exceeded')
+      },
     })
+  }
+
+  public ngOnDestroy(): void {
+    if (this._sub$) {
+      this._sub$.unsubscribe()
+    }
   }
 }
